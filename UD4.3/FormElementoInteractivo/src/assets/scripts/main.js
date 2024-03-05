64a1ee0ff524239cb2d9970db50a045888fc02a2
Jonathan <jleocan773@g.educaand.es>
2024-03-05 14:52:40 +0100

    JLC | 2024-03-05 | Elementos interactivos

--- UD4.3/FormElementoInteractivo/src/assets/scripts/main.js
+++ UD4.3/FormElementoInteractivo/src/assets/scripts/main.js
@@ -1,6 +1,9 @@
 //Función para activar y desactivar elementos al hacer clic en ellos
 
 function onIniciar() {
+
+  //Código de Tabs Activas
+  
   //Conseguir todas los botones de las pestañas
   const botonesTab = document.querySelectorAll(".botonesTab__pestana");
   const bloquesTexto = document.querySelectorAll(".contenedorTexto__bloque");
@@ -19,8 +22,23 @@ function onIniciar() {
       bloquesTexto[i].classList.add("activo");
     });
   });
+
+  //Código del Acordeón
+  //1º Seleccionamos todos los elementos sobre los que queremos trabajar
+  //h2Titulo es sobre el que haremos clic
+  const h2Titulo = document.querySelectorAll(".divContenedor__titulo");
+  //pContenido es el que modificaremos
+  const pContenido = document.querySelectorAll(".divContenedor__contenido");
+
+  h2Titulo.forEach((nodo, i) => {
+    nodo.addEventListener("click", () => {
+      if (pContenido[i].classList.contains("divContenedor__contenido--activo")) {
+        pContenido[i].classList.remove("divContenedor__contenido--activo");
+      } else {
+        pContenido[i].classList.add("divContenedor__contenido--activo");
+      }
+    });
+  });
 }
 
 window.onload = onIniciar();
-
-
